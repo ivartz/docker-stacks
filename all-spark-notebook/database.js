a10dd922bb47d6d0fdbfb61c42bc15a4ebd84fa3
ivartz <djloek@gmail.com>
2018-02-21 19:22:28 +0100

    install lighning-viz inside notebook. Inside notebook, start terminal. In terminal, start postgres with sudo /etc/init.d/postgresql start . Then cd lightning and npm start. Access the server with localhost:3000

---
+++ all-spark-notebook/database.js
@@ -0,0 +1,56 @@
+
+var url = require('url');
+var config = require('./config');
+
+var dbUrl = null;
+if(process.env.DATABASE_URL) {
+    dbUrl = url.parse(process.env.DATABASE_URL);
+}
+
+module.exports = {
+    'development': {
+        database: (dbUrl) ? dbUrl.path.replace('/', '') : 'lightning-viz',
+        username: (dbUrl) ? (dbUrl.auth.split(':') || [false])[0] : null,
+        password: (dbUrl) ? (dbUrl.auth.split(':') || [false])[1] : null,
+        'host': '127.0.0.1',
+        'dialect': 'sqlite',
+        'port': 5432,
+        'sync': {'force': true},
+        'storage': config.root + '/database.sqlite',
+        'logging': false
+    },
+    'test': {
+        database: (dbUrl) ? dbUrl.path.replace('/', '') : 'lightning-viz',
+        username: (dbUrl) ? (dbUrl.auth.split(':') || [false])[0] : null,
+        password: (dbUrl) ? (dbUrl.auth.split(':') || [false])[1] : null,
+        'host': '127.0.0.1',
+        'dialect': 'postgres',
+        'port': 5432,
+        'sync': {'force': true},
+        'storage': config.root + '/database.sqlite',
+        'logging': false
+    },
+    'test-sqlite': {
+        database: (dbUrl) ? dbUrl.path.replace('/', '') : 'lightning-viz',
+        username: (dbUrl) ? (dbUrl.auth.split(':') || [false])[0] : null,
+        password: (dbUrl) ? (dbUrl.auth.split(':') || [false])[1] : null,
+        'host': '127.0.0.1',
+        'dialect': 'sqlite',
+        'port': 5432,
+        'sync': {'force': true},
+        'storage': config.root + '/database.sqlite',
+        'logging': false
+    },
+    'production': {
+        database: (dbUrl) ? dbUrl.path.replace('/', '') : 'lightning-viz',
+        username: (dbUrl) ? (dbUrl.auth.split(':') || [false])[0] : 'jovyan',
+        password: (dbUrl) ? (dbUrl.auth.split(':') || [false])[1] : 'password',
+        host: (dbUrl) ? dbUrl.hostname : '127.0.0.1',
+        port: (dbUrl) ? dbUrl.port : 5432,
+        'sync': {'force': true},
+        'logging': false,
+        native: true,
+        ssl: true,
+        'dialect': 'postgres'
+    }
+};
